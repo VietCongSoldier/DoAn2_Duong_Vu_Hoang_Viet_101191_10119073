6273ffd9b3e00851a298856be8a2d5aef1c59234
Hoàng Việt <70848809+VietCongSoldier@users.noreply.github.com>
2021-05-28 03:21:15 +0700

    DVHV_10119073_101191_TK17.1

---
+++ slide_source.js
@@ -0,0 +1,23 @@
+var list = document.getElementsByClassName("slidejs");
+var index = 0;
+for(x of list){
+    x.style.display = 'none';
+}
+list[index].style.display='block';
+function showleft(){
+    for(x of list){
+        x.style.display='none';
+    }
+    if(index==0) index = list.length-1;
+    else index=index-1;
+    list[index].style.display='block';
+
+}function showright(){
+    for(x of list){
+        x.style.display='none';
+    }
+    if(index==list.length-1) index = 0;
+    else index=index+1;
+    list[index].style.display='block';
+}
+setTimeout(showleft,5000);
